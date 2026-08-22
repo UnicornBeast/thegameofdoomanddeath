45efa779797858fcce3cae46913d6055b164316b
UnicornBeast <aabbie100@gmail.com>
2026-08-22 15:14:03 -0400

    Added main menu + Credits

---
+++ Credits.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var sadR = $Label2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("depression"):
+		sadR.visible = true
